b644191d7806ca4a9747389fb35ba92902e92d67
Laptop <jdkyaz@gmail.com>
2024-06-29 22:18:39 -0700

    Adding proper API routing to be able to do get, post, and delete methods.

--- server.js
+++ server.js
@@ -19,6 +19,55 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+ // API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred while reading the notes');
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred while reading the notes');
+    }
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while saving the note');
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred while reading the notes');
+    }
+    let notes = JSON.parse(data);
+    notes = notes.filter(note => note.id !== noteId);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while deleting the note');
+      }
+      res.json({ ok: true });
+    });
+  });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
